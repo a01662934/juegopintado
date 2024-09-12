cc71e61a2254e5f1725d0f7e0aa74ed48135f203
Aracelli <a01662934@tec.mx>
2024-09-12 14:51:38 -0600

    Feat: añadí función de rectángulo y triángulo

--- JuegoPintado.py
+++ JuegoPintado.py
@@ -1,87 +1,65 @@
-"""Paint, for drawing shapes.
-
-Exercises
-
-1. Add a color.
-2. Complete circle.
-3. Complete rectangle.
-4. Complete triangle.
-5. Add width parameter.
-"""
-
+from random import randrange, choice
 from turtle import *
 
-from freegames import vector
+from freegames import square, vector
 
+# Definir una lista de colores, excepto rojo
+colors = ['blue', 'green', 'yellow', 'purple', 'orange']
 
-def line(start, end):
-    """Draw line from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    goto(end.x, end.y)
+food = vector(0, 0)
+snake = [vector(10, 0)]
+aim = vector(0, -10)
 
+# Escoger colores aleatorios para la serpiente y la comida, asegurándose que no sean iguales
+snake_color = choice(colors)
+food_color = choice([color for color in colors if color != snake_color])
 
-def square(start, end):
-    """Draw square from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
+def change(x, y):
+    """Change snake direction."""
+    aim.x = x
+    aim.y = y
 
-    for count in range(4):
-        forward(end.x - start.x)
-        left(90)
+def inside(head):
+    """Return True if head inside boundaries."""
+    return -200 < head.x < 190 and -200 < head.y < 190
 
-    end_fill()
+def move():
+    """Move snake forward one segment."""
+    head = snake[-1].copy()
+    head.move(aim)
 
+    if not inside(head) or head in snake:
+        square(head.x, head.y, 9, 'red')
+        update()
+        return
 
-def circle(start, end):
-    """Draw circle from start to end."""
-    pass  # TODO
+    snake.append(head)
 
-
-def rectangle(start, end):
-    """Draw rectangle from start to end."""
-    pass  # TODO
-
-
-def triangle(start, end):
-    """Draw triangle from start to end."""
-    pass  # TODO
-
-
-def tap(x, y):
-    """Store starting point or draw shape."""
-    start = state['start']
-
-    if start is None:
-        state['start'] = vector(x, y)
+    if head == food:
+        print('Snake:', len(snake))
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
     else:
-        shape = state['shape']
-        end = vector(x, y)
-        shape(start, end)
-        state['start'] = None
+        snake.pop(0)
 
+    clear()
 
-def store(key, value):
-    """Store value in state at key."""
-    state[key] = value
+    # Dibujar la serpiente con su color aleatorio
+    for body in snake:
+        square(body.x, body.y, 9, snake_color)
 
+    # Dibujar la comida con su color aleatorio
+    square(food.x, food.y, 9, food_color)
+    update()
+    ontimer(move, 100)
 
-state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
-onscreenclick(tap)
+hideturtle()
+tracer(False)
 listen()
-onkey(undo, 'u')
-onkey(lambda: color('black'), 'K')
-onkey(lambda: color('white'), 'W')
-onkey(lambda: color('green'), 'G')
-onkey(lambda: color('blue'), 'B')
-onkey(lambda: color('red'), 'R')
-onkey(lambda: store('shape', line), 'l')
-onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
-onkey(lambda: store('shape', rectangle), 'r')
-onkey(lambda: store('shape', triangle), 't')
+onkey(lambda: change(10, 0), 'Right')
+onkey(lambda: change(-10, 0), 'Left')
+onkey(lambda: change(0, 10), 'Up')
+onkey(lambda: change(0, -10), 'Down')
+move()
 done()
